4d0b6afa57023cfcf6a9e1a755fecb6f36ba41d6
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2025-05-02 23:07:30 +0530

    Create 01-helloworld.tf

---
+++ 01-HelloWorld/01-helloworld.tf
@@ -0,0 +1,10 @@
+output hello1 {
+    value = "Hello world"
+}
+
+#################### Output ####################
+ajinkya@hcl-rapid-platform:~$ terraform plan
+
+Changes to Outputs:
+  + hello1 = "Hello world"
+################################################
